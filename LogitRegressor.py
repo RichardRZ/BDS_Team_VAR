0161cbb617fd995123e0f7aecaeccdf251f442bd
Salts <altan_allawala@brown.edu>
2017-03-04 17:29:42 -0500

    snarf

--- LogitRegressor.py
+++ LogitRegressor.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import pandas as pd
 import datetime as dt
 from datetime import date
@@ -10,7 +8,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn import preprocessing
 
 # Datathon project with Trip Advisor dataset by Richen Zhang, Vikram Saraph, Altan Allawala.
-
 
 def saveData(df):
 	df.to_pickle("datathon_pickled")
@@ -37,13 +34,26 @@ del df['p_TotalPrice']
 df['day'] = pd.to_datetime(df['day'])
 df['weekday'] = df['day'].apply(lambda x: x.weekday())
 
-minimumDate = df['day'].min(axis=1)
+minimumDate = df['day'].min(axis=0)
 
 df['day'] = (pd.to_datetime(df['day']) - minimumDate)
 #df['day'] = df['day'].apply(lambda x: x.days)
 
 df['p_trafficChannel'] = df['p_trafficChannel'].apply(lambda x: ord(x))
 
+del df['day']
+
+C = 0.2
+X = df.ix[:,0:11].as_matrix()
+X = preprocessing.scale(X)
 print df.head(3)
 
-#print df
+Y = df.ix[:,11].as_matrix()
+#classifier = LogisticRegression(C = C, penalty = 'l1')
+#classifier.fit(X, Y)
+#coef_l1 = classifier.coef_.ravel()
+#print coef_l1
+#print X
+
+#print df.ix['BookingPurchase']
+#print df.head(3)
